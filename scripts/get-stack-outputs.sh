69e2fb5bf85dc5036c688cb17c9209c1b0b29191
Thomas Nixon <thomas@baselinejs.com>
2024-03-12 17:04:51 +1100

    Removed license tracker

--- scripts/get-stack-outputs.sh
+++ scripts/get-stack-outputs.sh
@@ -74,13 +74,9 @@ else
   fi
 fi
 
-LICENSE_ID="$(aws lambda list-functions --profile "$AWS_PROFILE" --region us-east-1 --output text --query 'Functions[].[FunctionArn]' | grep BaselineScheduledLambda | xargs -I {} aws lambda list-tags --profile "$AWS_PROFILE" --region us-east-1 --resource {} --query '{"{}":Tags}' | jq -r '.[] | .LicenseId')"
-export LICENSE_ID
-
 AWS_ACCOUNT_ID="$(aws sts get-caller-identity --profile "$AWS_PROFILE" --region us-east-1 --output text --query 'Account')"
 export AWS_ACCOUNT_ID
 
-echo LICENSE_ID="$LICENSE_ID"
 echo AWS_ACCOUNT_ID="$AWS_ACCOUNT_ID"
 
 end=$(date +%s)
