7b106385632815ffcd389636510cb56a11d41bed
Thomas Nixon <cant.get.nixon@gmail.com>
2024-03-15 11:56:58 +1100

    Update stylelint

--- packages/admin/src/baseblocks/user/components/user-settings/UserSettings.tsx
+++ packages/admin/src/baseblocks/user/components/user-settings/UserSettings.tsx
@@ -32,6 +32,7 @@ const UserSettings = (props: Props): JSX.Element => {
       await getUserDetails();
       setIsLoading(false);
     })();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const handleEmailChange = async () => {
